36142692d1225274a70e8d5ea91efba5cdbe616a
John Cross <jwcross1@dmacc.edu>
2019-05-20 12:05:09 -0500

    first commit

---
+++ src/main/java/edu/dmacc/codedsm/myownfizzbuzz/RandomNumber.java
@@ -0,0 +1,12 @@
+package edu.dmacc.codedsm.myownfizzbuzz;
+
+public class RandomNumber {
+
+    public double returnRandomNumber() {
+        double x = (int)(Math.random()*((1000000 - 0)+1))+0;
+    return x;
+    }
+
+
+
+}
